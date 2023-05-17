723ef94f8707c36881667d9024a17ca372094843
Gagandeep <igagandeep95@gmail.com>
2023-05-16 20:59:40 -0400

    refactored code :tada::tada:

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,13 @@ const Footer = () => {
   return (
     <footer>
       <div className="wrapper">
-        <p>Created at Juno College &copy;2023</p>
+        <p>
+          {" "}
+          <a href="https://junocollege.com/" target="_blank" rel="noreferrer">
+            Created at Juno College
+          </a>
+          &copy;2023
+        </p>
       </div>
     </footer>
   );
