04469359a13883702f7da87aeb69fcc71712a363
Gagandeep <igagandeep95@gmail.com>
2026-01-18 20:53:05 -0400

    feat: Implement initial application structure with routing, movie context, global styles, and a loading spinner.

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,11 @@
+import "./Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="netflix-loader-overlay">
+      <div className="netflix-loader-spinner"></div>
+    </div>
+  );
+};
+
+export default Loader;
